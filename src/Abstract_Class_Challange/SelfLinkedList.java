8cda296e3b0c8229d07408c38d719e1108d7eb7c
mohitjayant <mohitj8790@gmail.com>
2020-08-06 08:14:33 +0530

    Corrected file

--- src/Abstract_Class_Challange/SelfLinkedList.java
+++ src/Abstract_Class_Challange/SelfLinkedList.java
@@ -1,6 +1,7 @@
 package Abstract_Class_Challange;
 
 public class SelfLinkedList implements NodeList {
+
     private ListItem root=null;
 
     public SelfLinkedList(ListItem root) {
@@ -20,37 +21,34 @@ public class SelfLinkedList implements NodeList {
             return true;
         }
 
-        ListItem currentItem=this.root;
-        while (currentItem!=null) {
-            int comparision=currentItem.compareTo(newItem);
+        ListItem currentItem = this.root;
+        while (currentItem !=null) {
+            int comparision= (currentItem.compareTo(newItem));
             if (comparision<0) {
                 //currentItem is smaller than newItem,than move right
-                if(currentItem.next()!=null) {
-                    currentItem=currentItem.next();
+                if(currentItem.next() != null) {
+                    currentItem = currentItem.next();
                 }
                 else {
-                    currentItem.setNext(newItem);
-                    newItem.setPrevious(currentItem);
+                    currentItem.setNext(newItem).setPrevious(currentItem);
                     return true;
                 }
             }
             else if (comparision>0) {
                 if (currentItem.previous()!=null) {
-                    currentItem.previous().setNext(newItem);
-                    newItem.setPrevious(currentItem.previous());
-                    newItem.setNext(currentItem);
-                    currentItem.setNext(newItem);
+                    currentItem.previous().setNext(newItem).setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem).setPrevious(newItem);
                 }
                 else {
                     //previous to currentItem is root
-                    newItem.setNext(this.root);
-                    this.root.setPrevious(newItem);
+                    newItem.setNext(this.root).setPrevious(newItem);
                     this.root=newItem;
                 }
                 return true;
             }
             else {
                 System.out.println(newItem.getValue()+ " already present, can't added");
+                return false;
             }
         }
         return false;
