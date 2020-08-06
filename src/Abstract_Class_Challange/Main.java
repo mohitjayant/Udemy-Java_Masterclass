765401f99bb702fbe3d3d90187a40cfe064664b1
mohitjayant <mohitj8790@gmail.com>
2020-08-06 09:34:14 +0530

    Driven code file

--- src/Abstract_Class_Challange/Main.java
+++ src/Abstract_Class_Challange/Main.java
@@ -5,12 +5,26 @@ public class Main {
     public static void main(String[] args) {
         SelfLinkedList selfList=new SelfLinkedList(null);
         selfList.traverse(selfList.getRoot());
-        String stringData="Delhi Mumbai Pune Noida Bangalore Hyderabad Chennai Gurugram Jaipur Delhi";
+        String stringData="0 5 8 7 9 2 4 3 1 6";
         String[] data=stringData.split(" ");
         for (String s:data) {
             selfList.addItem(new Node(s));
         }
         selfList.traverse(selfList.getRoot());
+        selfList.removeItem(new Node("4"));
+        selfList.removeItem(new Node("2"));
+        selfList.removeItem(new Node("8"));
+        selfList.removeItem(new Node("7"));
+        selfList.removeItem(new Node("5"));
+        selfList.removeItem(new Node("3"));
+        selfList.removeItem(new Node("6"));
+        selfList.traverse(selfList.getRoot());
+        selfList.removeItem(new Node("9"));
+        selfList.removeItem(new Node("0"));
+        selfList.removeItem(selfList.getRoot());
+        selfList.traverse(selfList.getRoot());
+
+
     }
 
 }
