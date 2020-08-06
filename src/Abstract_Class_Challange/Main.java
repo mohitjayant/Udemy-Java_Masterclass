b7c6c15ad68cb4ad2b44f3657ad048fb8403b015
mohitjayant <mohitj8790@gmail.com>
2020-08-06 08:15:17 +0530

    Main file

--- src/Abstract_Class_Challange/Main.java
+++ src/Abstract_Class_Challange/Main.java
@@ -5,11 +5,12 @@ public class Main {
     public static void main(String[] args) {
         SelfLinkedList selfList=new SelfLinkedList(null);
         selfList.traverse(selfList.getRoot());
-        String stringData="Delhi Mumbai Pune Noida Bangalore Hyderabad Chennai Gurugram Jaipur";
+        String stringData="Delhi Mumbai Pune Noida Bangalore Hyderabad Chennai Gurugram Jaipur Delhi";
         String[] data=stringData.split(" ");
         for (String s:data) {
-            //System.out.println(s);
+            selfList.addItem(new Node(s));
         }
+        selfList.traverse(selfList.getRoot());
     }
 
 }
