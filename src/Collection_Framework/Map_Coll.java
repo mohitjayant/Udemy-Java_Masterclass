b7ddd4cdd92f0f1c392bb2a6620d77f394f2af8d
mohitjayant <mohitj8790@gmail.com>
2020-08-08 01:56:39 +0530

    Map interface implementation

--- src/Collection_Framework/Map_Coll.java
+++ src/Collection_Framework/Map_Coll.java
@@ -1,4 +1,31 @@
 package Collection_Framework;
 
-public class Map {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map_Coll {
+
+    public static void main(String[] args) {
+        Map<String,String> language=new HashMap<>();
+        language.put("1","Delhi");
+        language.put("2","Mumbai");
+        language.put("3","Jaipur");
+        language.put("4","Gurugram");
+        language.put("5","Pune");
+        language.put("2","Bandra");
+        //System.out.println(language);
+        language.remove("2");
+        if (language.remove("5","Pune")){
+            System.out.println("Key-Value Pair removed");
+        }
+        else {
+            System.out.println("Not removed");
+        }
+        System.out.println("=========================================================================");
+
+        for (String key:language.keySet()){
+            System.out.println(key+" : "+language.get(key));
+        }
+
+    }
 }
