10a8b2d91a1f9b7a5c4c6a407feefb5d6ed669bc
mohitjayant <mohitj8790@gmail.com>
2020-08-07 20:01:45 +0530

    Runner class of Theatre

--- src/TheatreProblem/Main.java
+++ src/TheatreProblem/Main.java
@@ -1,5 +1,9 @@
 package TheatreProblem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -17,5 +21,29 @@ public class Main {
         else {
             System.out.println("Sorry, Seat already taken");
         }
+        if (theatre.reserveSeat("D13")) {
+            System.out.println("Please pay for the seat");
+        }
+        else {
+            System.out.println("Sorry, Seat already taken");
+        }
+        List<Theatre.Seat> reverseSeat=new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeat);
+        printList(reverseSeat);
+
+        List<Theatre.Seat> priceSeat=new ArrayList<>(theatre.getSeats());
+        priceSeat.add(theatre.new Seat("B00",13.00));
+        priceSeat.add(theatre.new Seat("A00",13.00));
+        Collections.sort(priceSeat,Theatre.PRICE_ORDER);
+        printList(priceSeat);
+
+
+    }
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat:list) {
+            System.out.print(" " +seat.getSeatNumber() +" $"+seat.getPrice());
+        }
+        System.out.println();
+
     }
 }
