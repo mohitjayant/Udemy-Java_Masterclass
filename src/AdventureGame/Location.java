512eeaaf4e515ac0e5b9e86e37b7137fde055950
mohitjayant <mohitj8790@gmail.com>
2020-08-08 02:05:49 +0530

    Adventure Game

---
+++ src/AdventureGame/Location.java
@@ -0,0 +1,28 @@
+package AdventureGame;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationId;
+    private final String description;
+    private final Map<String,Integer> exits;
+
+    public Location(int locationId, String description) {
+        this.locationId = locationId;
+        this.description = description;
+        this.exits=new HashMap<String, Integer>();
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return exits;
+    }
+}
---
+++ src/AdventureGame/Main.java
@@ -0,0 +1,4 @@
+package AdventureGame;
+
+public class Main {
+}
