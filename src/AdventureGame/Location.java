32abbd00e446d46e2616881a4e6e982fc1356819
mohitjayant <mohitj8790@gmail.com>
2020-08-08 13:01:16 +0530

    Location class file

--- src/AdventureGame/Location.java
+++ src/AdventureGame/Location.java
@@ -8,15 +8,22 @@ public class Location {
     private final String description;
     private final Map<String,Integer> exits;
 
-    public Location(int locationId, String description) {
+    public Location(int locationId, String description,Map<String,Integer> exits) {
         this.locationId = locationId;
         this.description = description;
-        this.exits=new HashMap<String, Integer>();
+        if (exits!=null){
+            this.exits=new HashMap<String, Integer>(exits);
+        }
+        else {
+            this.exits=new HashMap<String, Integer>();
+        }
+        this.exits.put("Q",0);
+
     }
 
-    public void addExit(String direction,int location){
+/*    public void addExit(String direction,int location){
         exits.put(direction,location);
-    }
+    }*/
 
     public int getLocationId() {
         return locationId;
