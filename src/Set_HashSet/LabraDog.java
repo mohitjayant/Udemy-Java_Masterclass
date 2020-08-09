3d649239848405babb7729f2783ece45e066862e
mohitjayant <mohitj8790@gmail.com>
2020-08-09 11:22:08 +0530

    showing .equals(obj) method

--- src/Set_HashSet/LabraDog.java
+++ src/Set_HashSet/LabraDog.java
@@ -1,4 +1,21 @@
 package Set_HashSet;
 
-public class LabraDog {
+public class LabraDog extends Dog {
+
+    public LabraDog(String name) {
+        super(name);
+    }
+
+   /* @Override
+    public boolean equals(Object obj) {
+        if (this==obj){
+            return true;
+        }
+
+        if (obj instanceof  LabraDog){
+            String objName=((LabraDog) obj).getName();
+            return this.getName().equals(objName);
+        }
+        return false;
+    }*/
 }
