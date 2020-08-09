0832512106cc8f6f60937ab464b0d921b828f8b7
mohitjayant <mohitj8790@gmail.com>
2020-08-09 11:21:15 +0530

    showing .equals(obj) method

--- src/Set_HashSet/DogMain.java
+++ src/Set_HashSet/DogMain.java
@@ -1,4 +1,13 @@
 package Set_HashSet;
 
 public class DogMain {
+
+    public static void main(String[] args) {
+
+        LabraDog rover=new LabraDog("Rover");
+        Dog rover2=new Dog("Rover");
+
+        System.out.println(rover2.equals(rover));
+        System.out.println(rover.equals(rover2));
+    }
 }
