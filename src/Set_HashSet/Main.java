73ef9475b7633330f2dd4732ca0d28f7b394a2ae
mohitjayant <mohitj8790@gmail.com>
2020-08-09 10:11:13 +0530

    Driver class

--- src/Set_HashSet/Main.java
+++ src/Set_HashSet/Main.java
@@ -95,7 +95,16 @@ public class Main {
             System.out.println("\t" + moon.getName());
         }
 
+        HeavenlyBody pluto=new HeavenlyBody("Pluto",848);
+        planets.add(pluto);
 
+        for (HeavenlyBody planet:planets){
+            System.out.println(planet.getName()+" : "+planet.getOrbitalPeriod());
+        }
+
+        Object o=new Object();
+        o.equals(o);
+        "pluto".equals("");
 
     }
 }
