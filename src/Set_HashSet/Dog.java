b19f2ac570cb78f6c66ffd073057f4029a45003f
mohitjayant <mohitj8790@gmail.com>
2020-08-09 11:20:07 +0530

    showing .equals(obj) method

--- src/Set_HashSet/Dog.java
+++ src/Set_HashSet/Dog.java
@@ -1,4 +1,26 @@
 package Set_HashSet;
 
 public class Dog {
+    private final String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if (this==obj){
+            return true;
+        }
+
+        if (obj instanceof  Dog){
+            String objName=((Dog) obj).getName();
+            return this.name.equals(objName);
+        }
+        return false;
+    }
 }
