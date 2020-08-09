4d582bef20b3c0cf1d5e6ec2485f6c15e5a250c7
mohitjayant <mohitj8790@gmail.com>
2020-08-10 05:04:19 +0530

    Challange of Set

--- src/Set_Challange/Planet.java
+++ src/Set_Challange/Planet.java
@@ -1,4 +1,17 @@
 package Set_Challange;
 
-public class Planet {
+public class Planet extends HeavenlBody {
+    public Planet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.PLANET);
+    }
+
+    @Override
+    public boolean addSatellite(HeavenlBody moon) {
+        if (moon.getKey().getBodyType()==BodyTypes.MOON){
+            return super.addSatellite(moon);
+        }
+        else {
+            return false;
+        }
+    }
 }
