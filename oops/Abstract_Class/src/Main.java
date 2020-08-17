3d38b16bab19e20f7803ca5b3e4024d60fec60f7
mohitjayant <mohitj8790@gmail.com>
2020-08-17 17:04:46 +0530

    Abstract class example

---
+++ oops/Abstract_Class/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Dog  dog=new Dog("Yorkie");
+		dog.eat();
+		dog.breathe();
+		Parrot  parrot=new Parrot("Australian Ringneck");
+		parrot.eat();
+		parrot.breathe();
+		parrot.fly();
+		
+		Penguin penguin=new Penguin("Emperor");
+		penguin.fly();
+	}
+}
