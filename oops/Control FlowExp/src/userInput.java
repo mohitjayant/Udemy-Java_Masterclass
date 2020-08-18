d5a5733265b06a15babf48ccd4756318dd1248a4
mohitjayant <mohitj8790@gmail.com>
2020-08-18 11:47:44 +0530

    Control flow expression challanges solution

---
+++ oops/Control FlowExp/src/userInput.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class userInput
+{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int counter=0;
+        int sum=0;
+
+        while (true)
+        {
+            int order=counter+1;
+            System.out.println("Enter a number #"+order+":");
+            boolean isAnInt=scanner.hasNextInt();
+
+            if(isAnInt)
+            {
+                int number=scanner.nextInt();
+                counter++;
+                sum+=number;
+                if(counter==10)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                System.out.println("Invalid Number");
+            }
+            scanner.nextLine();
+
+        }
+        System.out.println("Sum= "+sum);
+
+        scanner.close();
+    }
+}
