6575b258c66d6ef5369bf3eb996a3af393d23ab3
mohitjayant <mohitj8790@gmail.com>
2020-08-18 11:37:19 +0530

    composition challange of udemy course

---
+++ oops/CompositionChallange/src/Wall.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author Mohit Jayant
+ *
+ */
+public class Wall {
+	private String directon;
+
+	public Wall(String directon) {
+		this.directon = directon;
+	}
+
+	public String getDirecton() {
+		return directon;
+	}
+	
+	
+	
+	
+}
